efc45324d2c7f77de17c43959c998a62f3902704
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-01-13 03:12:43 +0530

    home updaete

--- Home/index.php
+++ Home/index.php
@@ -10,8 +10,124 @@
     <?php include '../library/head.php'; ?>
     <!-- Head file -->
 
-
-    <center><h1>Home</h1></center>
-    
+    <!-- Area 1  -->
+    <div class="container" >
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <ul class="m-font" >
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Woman’s Fashion</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Men’s Fashion</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Electronics</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Home & Lifestyle</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Medicine</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Sports & Outdoor</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Baby’s & Toys</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Groceries & Pets</a>
+                    </li>
+                    <li class="home-sidemenu-li" >
+                        <a href="" class="home-a" >Health & Beauty</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-9">
+                <!-- carosal -->
+                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="../Images/Group 1000005941.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="../Images/Group 1000005942.png" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- Area 1 end -->
+    <!-- area 2 -->
+     <div class="container home-ara2">
+        <p class="small-title">Today's</p>
+        <h1 class="big-title" >Flash Sales</h1>
+            <div class="my-4">
+                <div class="row row-cols-1 row-cols-md-4 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                    <img src="../Images/Frame 570.png" class="card-img-top" alt="Card 1">
+                    <div class="card-body">
+                        <h5 class="card-title home-card-title">HAVIT HV-G92 Gamepad</h5>
+                        <p class="card-text home-card-mini-title">$120</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                    <img src="../Images/Frame 571.png" class="card-img-top" alt="Card 2">
+                    <div class="card-body">
+                        <h5 class="card-title home-card-title">AK-900 Wired Keyboard</h5>
+                        <p class="card-text home-card-mini-title">$960</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                    <img src="../Images/Frame 572.png" class="card-img-top" alt="Card 3">
+                    <div class="card-body">
+                        <h5 class="card-title home-card-title">IPS LCD Gaming Monitor</h5>
+                        <p class="card-text home-card-mini-title">$370</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                    <img src="../Images/Frame 573.png" class="card-img-top" alt="Card 4">
+                    <div class="card-body">
+                        <h5 class="card-title home-card-title">S-Series Comfort Chair </h5>
+                        <p class="card-text home-card-mini-title">$375</p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <button class="btn-view-all-product" >View All Products</button>
+            <br><br><hr>
+     </div>
+    <!-- area 2 end-->
+    <!-- area 3 -->
+     <div class="container home-ara2" >
+        <p class="small-title">Categories</p>
+        <h1 class="big-title" >Browse By Category</h1>
+        <br><br><hr>
+     </div>
+    <!-- area 3 end-->
+    <!-- area 4 -->
+    <div class="container home-ara2" >
+        <p class="small-title">This Month</p>
+        <h1 class="big-title" >Best Selling Products</h1>
+     </div>
+    <!-- area 4 end-->
 </body>
 </html>
--- Library/style.php
+++ Library/style.php
@@ -1,3 +1,22 @@
 <style>
 
+/* Theme */
+.m-font{font-family: 'Poppins', sans-serif;}
+.small-title{color:#DB4444; font-size: 20px; font-weight: bold;}
+.big-title{font-size: 36px; margin-top: -20px;}
+.home-card-title{font-size: 16px; font-weight: 600;}
+.home-card-mini-title{color: #DB4444; font-weight: 600;}
+
+
+body{font-family: 'Poppins', sans-serif;}
+
+
+
+/* Home */
+.home-a{text-decoration: none; color: black;}
+.home-sidemenu-li{list-style: none; margin-top: 17px;}
+.home-ara2{margin-top: 100px;}
+.rectangle {width: 20px;height: 60px;background-color: #E53935; border-radius: 8px; margin-right: 10px;}
+.btn-view-all-product{color: #fff; background-color: #DB4444; padding: 10px;padding-left: 20px; padding-right: 20px; border: #DB4444; border-radius: 5px;}
+
 </style>
