a60635e2f63f4db88eb12b9bc3407a322c10320b
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-01-14 03:03:18 +0530

    home

--- Library/style.php
+++ Library/style.php
@@ -18,5 +18,17 @@ body{font-family: 'Poppins', sans-serif;}
 .home-ara2{margin-top: 100px;}
 .rectangle {width: 20px;height: 60px;background-color: #E53935; border-radius: 8px; margin-right: 10px;}
 .btn-view-all-product{color: #fff; background-color: #DB4444; padding: 10px;padding-left: 20px; padding-right: 20px; border: #DB4444; border-radius: 5px;}
+.home-category-card{width: 30%; margin: auto; margin-top: 10px;}
+.home-category-text{text-align: center;}
+.home-sale-banner{background-color: black;padding: 50px; color:#fff;}
+.home-sale-banner-img{width: 100%; margin-bottom: -250px;}
+.home-services-card{width: 30%; margin: auto; margin-top: 10px;}
+.home-services-h5{text-align: center;}
+.home-services-text{text-align: center;}
+
+
+
+
+
 
 </style>
