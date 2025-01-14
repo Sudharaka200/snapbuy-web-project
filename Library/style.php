f6a08167bc8fa928cfe63593eb6c34d5e149b551
Dilukshan16 <pdiluksan@gmail.com>
2025-01-14 16:15:31 +0530

    signup commit

--- Library/style.php
+++ Library/style.php
@@ -1,3 +1,9 @@
 <style>
+    .Sign {
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css"><link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/registrations/registration-12/assets/css/registration-12.css">
+    }
 
+    .contact {
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css"><link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/contacts/contact-5/assets/css/contact-5.css">
+    }
 </style>
